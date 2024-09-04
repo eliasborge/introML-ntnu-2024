4477417f708e66a5d2a383f63589160070e8c18f
Elias Borge Svinø <94860474+eliasborge@users.noreply.github.com>
2024-09-04 14:58:02 +0200

    Add Mission 1

--- Assignment1/linear_regression.py
+++ Assignment1/linear_regression.py
@@ -1,36 +1,22 @@
 import numpy as np
-
+import pandas as pd
+from sklearn.metrics import r2_score
 class LinearRegression():
     
-    def __init__(self):
-        # NOTE: Feel free to add any hyperparameters 
-        # (with defaults) as you see fit
-        pass
-        
+    def __init__(self, learning_rate=0.001, n_iterations=5000):
+        self.learning_rate = learning_rate
+        self.n_iterations = n_iterations
+        self.theta_0 = 0  # Intercept
+        self.theta_1 = 0  # Slope
+
     def fit(self, X, y):
-        """
-        Estimates parameters for the classifier
-        
-        Args:
-            X (array<m,n>): a matrix of floats with
-                m rows (#samples) and n columns (#features)
-            y (array<m>): a vector of floats
-        """
-        # TODO: Implement
-        raise NotImplementedError("The fit method is not implemented yet.")
-    
+        m = len(y)
+        for _ in range(self.n_iterations):
+            y_pred = self.theta_0 + self.theta_1 * X
+            d_theta_0 = (1/m) * np.sum(y_pred - y)
+            d_theta_1 = (1/m) * np.sum((y_pred - y) * X)
+            self.theta_0 -= self.learning_rate * d_theta_0
+            self.theta_1 -= self.learning_rate * d_theta_1
+
     def predict(self, X):
-        """
-        Generates predictions
-        
-        Note: should be called after .fit()
-        
-        Args:
-            X (array<m,n>): a matrix of floats with 
-                m rows (#samples) and n columns (#features)
-            
-        Returns:
-            A length m array of floats
-        """
-        # TODO: Implement
-        raise NotImplementedError("The predict method is not implemented yet.")
+        return self.theta_0 + self.theta_1 * X
