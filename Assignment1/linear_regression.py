ece96faa0ca816519e8532fe4b1f8c44ce117c26
Elias Borge Svinø <94860474+eliasborge@users.noreply.github.com>
2024-09-04 15:34:27 +0200

    Add discussion for mission 1

--- Assignment1/linear_regression.py
+++ Assignment1/linear_regression.py
@@ -6,17 +6,18 @@ class LinearRegression():
     def __init__(self, learning_rate=0.001, n_iterations=5000):
         self.learning_rate = learning_rate
         self.n_iterations = n_iterations
-        self.theta_0 = 0  # Intercept
-        self.theta_1 = 0  # Slope
+        # y = ax+b
+        self.b = 0  # Intercept
+        self.a = 0  # Slope
 
     def fit(self, X, y):
         m = len(y)
         for _ in range(self.n_iterations):
-            y_pred = self.theta_0 + self.theta_1 * X
-            d_theta_0 = (1/m) * np.sum(y_pred - y)
-            d_theta_1 = (1/m) * np.sum((y_pred - y) * X)
-            self.theta_0 -= self.learning_rate * d_theta_0
-            self.theta_1 -= self.learning_rate * d_theta_1
+            y_pred = self.b + self.a * X
+            d_b = (1/m) * np.sum(y_pred - y)
+            d_a = (1/m) * np.sum((y_pred - y) * X)
+            self.b -= self.learning_rate * d_b
+            self.a -= self.learning_rate * d_a
 
     def predict(self, X):
-        return self.theta_0 + self.theta_1 * X
+        return self.b + self.a * X
