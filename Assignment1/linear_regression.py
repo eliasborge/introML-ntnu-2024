be179cf663b10565082a939988d0cdf504f38d74
Thomas Addisu Frette <thomas.frette@gmail.com>
2024-09-05 17:44:15 +0200

    Nesten ferdig, må justere learning rate og epoch

--- Assignment1/linear_regression.py
+++ Assignment1/linear_regression.py
@@ -16,8 +16,10 @@ class LinearRegression():
             y_pred = self.b + self.a * X
             d_b = (1/m) * np.sum(y_pred - y)
             d_a = (1/m) * np.sum((y_pred - y) * X)
+            cost = (1/m) * sum([val**2 for val in (y - y_pred)])
             self.b -= self.learning_rate * d_b
             self.a -= self.learning_rate * d_a
+            print("m {}, b {}, cost {}, iteration {}".format(d_a, d_b, cost, _))
 
     def predict(self, X):
         return self.b + self.a * X
