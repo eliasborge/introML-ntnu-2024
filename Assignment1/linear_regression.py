d444f144bd4ee3dd7712e761f2e0f0d73daa1020
Thomas Addisu Frette <thomas.frette@gmail.com>
2024-09-18 16:53:44 +0200

    finetune linear regression

--- Assignment1/linear_regression.py
+++ Assignment1/linear_regression.py
@@ -13,16 +13,24 @@ class LinearRegression():
         self.b = 0  # Intercept
         self.a = 0  # Slope
 
-    def fit(self, X, y):
+    def fit(self, X, y, tolerance=1e-6):
         m = len(y)
+        previous_cost = float('inf')
         for _ in range(self.n_iterations):
             y_pred = self.b + self.a * X
             d_b = (1/m) * np.sum(y_pred - y)
             d_a = (1/m) * np.sum((y_pred - y) * X)
-            cost = (1/m) * sum([val**2 for val in (y - y_pred)])
+            cost = (1/m) * np.sum((y_pred - y) ** 2)  # Mean squared error
+            
+            if abs(previous_cost - cost) < tolerance:  # Convergence check
+                print(f"Converged at iteration {_} with cost {cost}")
+                break
+            
+            previous_cost = cost
             self.b -= self.learning_rate * d_b
             self.a -= self.learning_rate * d_a
-            print("m {}, b {}, cost {}, iteration {}".format(d_a, d_b, cost, _))
+            print(f"Iteration {_}, cost: {cost}, a: {self.a}, b: {self.b}")
+    
 
     def predict(self, X):
         return self.b + self.a * X
